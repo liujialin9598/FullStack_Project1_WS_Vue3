6f71e00344dd1dd465166c06712a3884822d2655
Jialin Liu <mrliujalo@foxmail.com>
2024-06-27 14:54:58 +0800

    add google tag manager

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import App from './App.vue'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import App from "./App.vue";
+import { createGtm } from "@gtm-support/vue-gtm";
 
-import router from './router'
+import router from "./router";
 
-const app = createApp(App)
-app.use(ElementPlus, { size: 'small', zIndex: 3000 })
-app.use(router)
-app.mount('#app')
-
+const app = createApp(App);
+app.use(ElementPlus, { size: "small", zIndex: 3000 });
+app.use(router);
+app.use(
+  createGtm({
+    id: "GTM-PTPXKXTV",
+    vueRouter: router,
+  })
+);
+app.mount("#app");
